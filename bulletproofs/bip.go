f3cf03bbab72dd6a2f64038b852a45e9f13c3b80
Eduardo Morais <eduardo.morais@gmail.com>
2019-06-17 11:45:50 +0200

    Include comments to indicate equations for ZKIP as in the paper

--- bulletproofs/bip.go
+++ bulletproofs/bip.go
@@ -118,56 +118,56 @@ func computeBipRecursive(a, b []*big.Int, g, h []*p256.P256, u, P *p256.P256, n 
 		// recursion
 
 		// nprime := n / 2
-		nprime := n / 2
+		nprime := n / 2                                                       // (20)
 
-		// Compute cL = < a[:n'], b[n':] >
+		// Compute cL = < a[:n'], b[n':] >                                    // (21)
 		cL, _ = ScalarProduct(a[:nprime], b[nprime:])
-		// Compute cR = < a[n':], b[:n'] >
+		// Compute cR = < a[n':], b[:n'] >                                    // (22) 
 		cR, _ = ScalarProduct(a[nprime:], b[:nprime])
-		// Compute L = g[n':]^(a[:n']).h[:n']^(b[n':]).u^cL
+		// Compute L = g[n':]^(a[:n']).h[:n']^(b[n':]).u^cL                   // (23)
 		L, _ = VectorExp(g[nprime:], a[:nprime])
 		Lh, _ = VectorExp(h[:nprime], b[nprime:])
 		L.Multiply(L, Lh)
 		L.Multiply(L, new(p256.P256).ScalarMult(u, cL))
 
-		// Compute R = g[:n']^(a[n':]).h[n':]^(b[:n']).u^cR
+		// Compute R = g[:n']^(a[n':]).h[n':]^(b[:n']).u^cR                   // (24)
 		R, _ = VectorExp(g[:nprime], a[nprime:])
 		Rh, _ = VectorExp(h[nprime:], b[:nprime])
 		R.Multiply(R, Rh)
 		R.Multiply(R, new(p256.P256).ScalarMult(u, cR))
 
-		// Fiat-Shamir:
+		// Fiat-Shamir:                                                       // (26)
 		x, _, _ = HashBP(L, R)
 		xinv = bn.ModInverse(x, ORDER)
 
-		// Compute g' = g[:n']^(x^-1) * g[n':]^(x)
+		// Compute g' = g[:n']^(x^-1) * g[n':]^(x)                            // (29)
 		gprime, _ = VectorScalarExp(g[:nprime], xinv)
 		gprime2, _ = VectorScalarExp(g[nprime:], x)
 		gprime, _ = VectorECAdd(gprime, gprime2)
-		// Compute h' = h[:n']^(x)    * h[n':]^(x^-1)
+		// Compute h' = h[:n']^(x)    * h[n':]^(x^-1)                         // (30)
 		hprime, _ = VectorScalarExp(h[:nprime], x)
 		hprime2, _ = VectorScalarExp(h[nprime:], xinv)
 		hprime, _ = VectorECAdd(hprime, hprime2)
 
-		// Compute P' = L^(x^2).P.R^(x^-2)
+		// Compute P' = L^(x^2).P.R^(x^-2)                                    // (31)
 		x2 = bn.Mod(bn.Multiply(x, x), ORDER)
 		x2inv = bn.ModInverse(x2, ORDER)
 		Pprime = new(p256.P256).ScalarMult(L, x2)
 		Pprime.Multiply(Pprime, P)
 		Pprime.Multiply(Pprime, new(p256.P256).ScalarMult(R, x2inv))
 
-		// Compute a' = a[:n'].x      + a[n':].x^(-1)
+		// Compute a' = a[:n'].x      + a[n':].x^(-1)                         // (33)
 		aprime, _ = VectorScalarMul(a[:nprime], x)
 		aprime2, _ = VectorScalarMul(a[nprime:], xinv)
 		aprime, _ = VectorAdd(aprime, aprime2)
-		// Compute b' = b[:n'].x^(-1) + b[n':].x
+		// Compute b' = b[:n'].x^(-1) + b[n':].x                              // (34)
 		bprime, _ = VectorScalarMul(b[:nprime], xinv)
 		bprime2, _ = VectorScalarMul(b[nprime:], x)
 		bprime, _ = VectorAdd(bprime, bprime2)
 
 		Ls = append(Ls, L)
 		Rs = append(Rs, R)
-		// recursion computeBipRecursive(g',h',u,P'; a', b')
+		// recursion computeBipRecursive(g',h',u,P'; a', b')                  // (35)
 		proof = computeBipRecursive(aprime, bprime, gprime, hprime, u, Pprime, nprime, Ls, Rs)
 	}
 	proof.N = n
@@ -192,18 +192,18 @@ func (zkip *bip) Verify(proof proofBip) (bool, error) {
 	Pprime := zkip.P
 	nprime := proof.N
 	for i < int64(logn) {
-		nprime = nprime / 2
-		x, _, _ = HashBP(proof.Ls[i], proof.Rs[i])
+		nprime = nprime / 2                                                   // (20)
+		x, _, _ = HashBP(proof.Ls[i], proof.Rs[i])                            // (26)
 		xinv = bn.ModInverse(x, ORDER)
-		// Compute g' = g[:n']^(x^-1) * g[n':]^(x)
+		// Compute g' = g[:n']^(x^-1) * g[n':]^(x)                            // (29)
 		ngprime, _ = VectorScalarExp(gprime[:nprime], xinv)
 		ngprime2, _ = VectorScalarExp(gprime[nprime:], x)
 		gprime, _ = VectorECAdd(ngprime, ngprime2)
-		// Compute h' = h[:n']^(x)    * h[n':]^(x^-1)
+		// Compute h' = h[:n']^(x)    * h[n':]^(x^-1)                         // (30)
 		nhprime, _ = VectorScalarExp(hprime[:nprime], x)
 		nhprime2, _ = VectorScalarExp(hprime[nprime:], xinv)
 		hprime, _ = VectorECAdd(nhprime, nhprime2)
-		// Compute P' = L^(x^2).P.R^(x^-2)
+		// Compute P' = L^(x^2).P.R^(x^-2)                                    // (31)
 		x2 = bn.Mod(bn.Multiply(x, x), ORDER)
 		x2inv = bn.ModInverse(x2, ORDER)
 		Pprime.Multiply(Pprime, new(p256.P256).ScalarMult(proof.Ls[i], x2))
@@ -211,17 +211,18 @@ func (zkip *bip) Verify(proof proofBip) (bool, error) {
 		i = i + 1
 	}
 
-	// c == a*b
+	// c == a*b and checks if P = g^a.h^b.u^c                                     // (16)
 	ab := bn.Multiply(proof.A, proof.B)
 	ab = bn.Mod(ab, ORDER)
-
+	// Compute right hand side
 	rhs := new(p256.P256).ScalarMult(gprime[0], proof.A)
 	hb := new(p256.P256).ScalarMult(hprime[0], proof.B)
 	rhs.Multiply(rhs, hb)
 	rhs.Multiply(rhs, new(p256.P256).ScalarMult(proof.U, ab))
-
+	// Compute inverse of left hand side
 	nP := Pprime.Neg(Pprime)
 	nP.Multiply(nP, rhs)
+	// If both sides are equal then nP must be zero                               // (17)
 	c := nP.IsZero()
 
 	return c, nil
