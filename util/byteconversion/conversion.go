85656262a8035550a3e0e87f298916f0ed9a1f0b
Matthijs van den Bos <matthijs.van.den.bos@ing.com>
2019-07-22 13:52:59 +0200

    Update licens and add copyright headers

--- util/byteconversion/conversion.go
+++ util/byteconversion/conversion.go
@@ -1,24 +1,25 @@
-// Copyright 2017 ING Bank N.V.
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+/*
+ * Copyright (C) 2019. ING BANK N.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package byteconversion
 
 import (
-	"errors"
-	"math/big"
+    "errors"
+    "math/big"
 )
 
 /**
@@ -29,35 +30,35 @@ import (
  */
 func FromByteArray(bytesIn []byte) (*big.Int, error) {
 
-	const MINUS_ONE = -1
+    const MINUS_ONE = -1
 
-	if len(bytesIn) == 0 {
-		err := errors.New("Cannot convert empty array to big.Int.")
-		return nil, err
-	}
+    if len(bytesIn) == 0 {
+        err := errors.New("Cannot convert empty array to big.Int.")
+        return nil, err
+    }
 
-	highestByte := bytesIn[0]
-	isNegative := (highestByte & 128) != 0
-	var convertedBytes []byte
+    highestByte := bytesIn[0]
+    isNegative := (highestByte & 128) != 0
+    var convertedBytes []byte
 
-	if isNegative {
+    if isNegative {
 
-		tmpInt := new(big.Int).SetBytes(bytesIn)
-		tmpInt = tmpInt.Sub(tmpInt, big.NewInt(1))
-		tmpBytes := tmpInt.Bytes()
+        tmpInt := new(big.Int).SetBytes(bytesIn)
+        tmpInt = tmpInt.Sub(tmpInt, big.NewInt(1))
+        tmpBytes := tmpInt.Bytes()
 
-		if tmpBytes[0] == 255 {
-			convertedBytes = FlipBytes(tmpBytes)[1:]
-		} else {
-			convertedBytes = tmpBytes
-			copy(convertedBytes, FlipBytes(tmpBytes))
-		}
-		tmp := new(big.Int).SetBytes(convertedBytes)
-		return tmp.Mul(tmp, big.NewInt(MINUS_ONE)), nil
-	} else {
-		// if positive leave unchanged (additional 0-bytes will be ignored)
-		return new(big.Int).SetBytes(bytesIn), nil
-	}
+        if tmpBytes[0] == 255 {
+            convertedBytes = FlipBytes(tmpBytes)[1:]
+        } else {
+            convertedBytes = tmpBytes
+            copy(convertedBytes, FlipBytes(tmpBytes))
+        }
+        tmp := new(big.Int).SetBytes(convertedBytes)
+        return tmp.Mul(tmp, big.NewInt(MINUS_ONE)), nil
+    } else {
+        // if positive leave unchanged (additional 0-bytes will be ignored)
+        return new(big.Int).SetBytes(bytesIn), nil
+    }
 }
 
 /**
@@ -68,42 +69,42 @@ func FromByteArray(bytesIn []byte) (*big.Int, error) {
  */
 func ToByteArray(in *big.Int) []byte {
 
-	isNegative := in.Cmp(new(big.Int).SetInt64(0)) < 0
+    isNegative := in.Cmp(new(big.Int).SetInt64(0)) < 0
 
-	bytes := in.Bytes()
-	length := len(bytes)
+    bytes := in.Bytes()
+    length := len(bytes)
 
-	if length == 0 {
-		return []byte{0}
-	}
+    if length == 0 {
+        return []byte{0}
+    }
 
-	highestByte := bytes[0]
-	var convertedBytes []byte
+    highestByte := bytes[0]
+    var convertedBytes []byte
 
-	if !isNegative {
-		if (highestByte & 128) != 0 {
+    if !isNegative {
+        if (highestByte & 128) != 0 {
 
-			convertedBytes = make([]byte, length+1)
-			convertedBytes[0] = 0
-			copy(convertedBytes[1:], bytes)
-			return convertedBytes
-		} else {
-			return bytes
-		}
-	} else {
-		if (highestByte & 128) != 0 {
+            convertedBytes = make([]byte, length+1)
+            convertedBytes[0] = 0
+            copy(convertedBytes[1:], bytes)
+            return convertedBytes
+        } else {
+            return bytes
+        }
+    } else {
+        if (highestByte & 128) != 0 {
 
-			convertedBytes = make([]byte, length+1)
-			convertedBytes[0] = 255
-			copy(convertedBytes[1:], FlipBytes(bytes))
-		} else {
-			convertedBytes = FlipBytes(bytes)
-		}
+            convertedBytes = make([]byte, length+1)
+            convertedBytes[0] = 255
+            copy(convertedBytes[1:], FlipBytes(bytes))
+        } else {
+            convertedBytes = FlipBytes(bytes)
+        }
 
-		convertedInt := new(big.Int).SetBytes(convertedBytes)
-		convertedInt.Add(convertedInt, big.NewInt(1))
-		return convertedInt.Bytes()
-	}
+        convertedInt := new(big.Int).SetBytes(convertedBytes)
+        convertedInt.Add(convertedInt, big.NewInt(1))
+        return convertedInt.Bytes()
+    }
 }
 
 /**
@@ -112,11 +113,11 @@ func ToByteArray(in *big.Int) []byte {
  */
 func FlipBytes(bytesIn []byte) []byte {
 
-	length := len(bytesIn)
-	flippedBytes := make([]byte, length)
+    length := len(bytesIn)
+    flippedBytes := make([]byte, length)
 
-	for i := 0; i < length; i++ {
-		flippedBytes[i] = bytesIn[i] ^ 255
-	}
-	return flippedBytes
+    for i := 0; i < length; i++ {
+        flippedBytes[i] = bytesIn[i] ^ 255
+    }
+    return flippedBytes
 }
--- util/util.go
+++ util/util.go
@@ -1,35 +1,37 @@
-// Copyright 2018 ING Bank N.V.
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+/*
+ * Copyright (C) 2019. ING BANK N.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package util
 
 import (
-	"crypto/sha256"
-	"github.com/mvdbos/zkpsdk/crypto/bn256"
-	"github.com/mvdbos/zkpsdk/crypto/p256"
-	"github.com/mvdbos/zkpsdk/util/bn"
-	"github.com/mvdbos/zkpsdk/util/byteconversion"
-	"math/big"
+    "crypto/sha256"
+    "math/big"
+
+    "github.com/mvdbos/zkpsdk/crypto/bn256"
+    "github.com/mvdbos/zkpsdk/crypto/p256"
+    "github.com/mvdbos/zkpsdk/util/bn"
+    "github.com/mvdbos/zkpsdk/util/byteconversion"
 )
 
-//Constants that are going to be used frequently, then we just need to compute them once.
+// Constants that are going to be used frequently, then we just need to compute them once.
 var (
-	G1 = new(bn256.G1).ScalarBaseMult(new(big.Int).SetInt64(1))
-	G2 = new(bn256.G2).ScalarBaseMult(new(big.Int).SetInt64(1))
-	E  = bn256.Pair(G1, G2)
+    G1 = new(bn256.G1).ScalarBaseMult(new(big.Int).SetInt64(1))
+    G2 = new(bn256.G2).ScalarBaseMult(new(big.Int).SetInt64(1))
+    E  = bn256.Pair(G1, G2)
 )
 
 /*
@@ -37,18 +39,18 @@ Decompose receives as input a bigint x and outputs an array of integers such tha
 x = sum(xi.u^i), i.e. it returns the decomposition of x into base u.
 */
 func Decompose(x *big.Int, u int64, l int64) ([]int64, error) {
-	var (
-		result []int64
-		i      int64
-	)
-	result = make([]int64, l)
-	i = 0
-	for i < l {
-		result[i] = bn.Mod(x, new(big.Int).SetInt64(u)).Int64()
-		x = new(big.Int).Div(x, new(big.Int).SetInt64(u))
-		i = i + 1
-	}
-	return result, nil
+    var (
+        result []int64
+        i      int64
+    )
+    result = make([]int64, l)
+    i = 0
+    for i < l {
+        result[i] = bn.Mod(x, new(big.Int).SetInt64(u)).Int64()
+        x = new(big.Int).Div(x, new(big.Int).SetInt64(u))
+        i = i + 1
+    }
+    return result, nil
 }
 
 /*
@@ -56,9 +58,9 @@ Commit method corresponds to the Pedersen commitment scheme. Namely, given input
 message x, and randomness r, it outputs g^x.h^r.
 */
 func Commit(x, r *big.Int, h *bn256.G2) (*bn256.G2, error) {
-	var C = new(bn256.G2).ScalarBaseMult(x)
-	C.Add(C, new(bn256.G2).ScalarMult(h, r))
-	return C, nil
+    var C = new(bn256.G2).ScalarBaseMult(x)
+    C.Add(C, new(bn256.G2).ScalarMult(h, r))
+    return C, nil
 }
 
 /*
@@ -66,34 +68,34 @@ CommitG1 method corresponds to the Pedersen commitment scheme. Namely, given inp
 message x, and randomness r, it outputs g^x.h^r.
 */
 func CommitG1(x, r *big.Int, h *p256.P256) (*p256.P256, error) {
-	var C = new(p256.P256).ScalarBaseMult(x)
-	Hr := new(p256.P256).ScalarMult(h, r)
-	C.Add(C, Hr)
-	return C, nil
+    var C = new(p256.P256).ScalarBaseMult(x)
+    Hr := new(p256.P256).ScalarMult(h, r)
+    C.Add(C, Hr)
+    return C, nil
 }
 
 /*
 HashSet is responsible for the computing a Zp element given elements from GT and G2.
 */
 func HashSet(a *bn256.GT, D *bn256.G2) (*big.Int, error) {
-	digest := sha256.New()
-	digest.Write([]byte(a.String()))
-	digest.Write([]byte(D.String()))
-	output := digest.Sum(nil)
-	tmp := output[0:]
-	return byteconversion.FromByteArray(tmp)
+    digest := sha256.New()
+    digest.Write([]byte(a.String()))
+    digest.Write([]byte(D.String()))
+    output := digest.Sum(nil)
+    tmp := output[0:]
+    return byteconversion.FromByteArray(tmp)
 }
 
 /*
 Hash is responsible for the computing a Zp element given elements from GT and G2.
 */
 func Hash(a []*bn256.GT, D *bn256.G2) (*big.Int, error) {
-	digest := sha256.New()
-	for i := range a {
-		digest.Write([]byte(a[i].String()))
-	}
-	digest.Write([]byte(D.String()))
-	output := digest.Sum(nil)
-	tmp := output[0:]
-	return byteconversion.FromByteArray(tmp)
+    digest := sha256.New()
+    for i := range a {
+        digest.Write([]byte(a[i].String()))
+    }
+    digest.Write([]byte(D.String()))
+    output := digest.Sum(nil)
+    tmp := output[0:]
+    return byteconversion.FromByteArray(tmp)
 }
