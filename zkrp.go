115fda88846f49c87dbe18eac058448bc291037f
Matthijs van den Bos <matthijs.van.den.bos@ing.com>
2019-07-22 15:40:48 +0200

    Update license header

--- zkrp.go
+++ zkrp.go
@@ -1,18 +1,19 @@
-// Copyright 2018 ING Bank N.V.
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+/*
+ * Copyright (C) 2019 ING BANK N.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 /*
 This file contains the implementation of the ZKRP scheme proposed in the paper:
@@ -23,9 +24,9 @@ Asiacrypt 2008
 
 package zkpsdk
 
-//type izkrp interface {
-//	//Commit()
-//	SetupInnerProduct(a, b int64)
-//	Prove()
-//	Verify() (bool, error)
-//}
+// type izkrp interface {
+// 	//Commit()
+// 	SetupInnerProduct(a, b int64)
+// 	Prove()
+// 	Verify() (bool, error)
+// }
